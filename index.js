0aaf6d03e944d42ed496c4592a5c5f331c7b37c1
Wgbruce <brucewarm111@gmail.com>
2018-06-05 20:47:00 +0800

    update index.js
    將辨識結果寫入output.txt檔儲存。

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const server = express();
+const Tesseract = require('tesseract.js');
 
 var fs = require('fs');
 var formidable = require('formidable');
 
 // html file containing upload form
 var upload_html = fs.readFileSync("views\\index.html");
- 
+
 // replace this with the location to save uploaded files
 //var upload_path = "C:\\Users\\GAO-ZIE JIE\\Desktop\\jk\\";
-var upload_path = __dirname+"\\static\\file\\"
+var upload_path = __dirname + "\\static\\file\\"
 //server.use(cors({credentials: true, origin: true}));
 server.use(cors());
-server.use('/static', express.static(__dirname+'/static'));
+server.use('/static', express.static(__dirname + '/static'));
 server.use(express.static('file'));
 
 ///////////////////////////////////
@@ -29,17 +30,17 @@ const PORT = 8000;
 //          處理路由              //
 ///////////////////////////////////
 server.get('/', (req, res) => {
-	
+
     res.sendFile('index.html', {
         root: path.join(__dirname, './views/')
     });
 });
 
-server.post('/fileupload',(req,res)=>{
-	 if (req.url == '/uploadform') {
-      res.writeHead(200);
-      res.write(upload_html);
-      return res.end();
+server.post('/fileupload', (req, res) => {
+    if (req.url == '/uploadform') {
+        res.writeHead(200);
+        res.write(upload_html);
+        return res.end();
     } else if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
@@ -50,13 +51,41 @@ server.post('/fileupload',(req,res)=>{
             fs.rename(oldpath, newpath, function (err) {
                 if (err) throw err;
                 // you may respond with another html page
+
+                Tesseract.recognize(newpath, {
+                    lang: 'eng',
+                }).progress(function (message) {
+                    //console.log(message);
+                }).then(function (result) {
+                    //console.log(result);
+                    var block = result.blocks[0].bbox;
+                    var output = '圖片位置x0: ' + block.x0 + '\n';
+                    output += '圖片位置x1: ' + block.x1 + '\n';
+                    output += '圖片位置y0: ' + block.y0 + '\n';
+                    output += '圖片位置y1: ' + block.y1 + '\n';
+                    output += 'text: ' + result.text + '\n';
+                    console.log(output);
+					
+					var fss = require('fs');
+					fss.writeFile("output.txt", output, function(err) {
+						if(err) {
+							console.log(err);
+						} else {
+							console.log("The file was saved!");
+						}
+					});
+					
+                }).catch(function (error) {
+                    console.error(error);
+                });
+
                 res.write('File uploaded and moved!');
-				//res.write(oldpath);
-				res.write(newpath);
+                //res.write(oldpath);
+                res.write(newpath);
                 res.end();
             });
         });
-    } 
+    }
 });
 
 server.listen(PORT, () => {
